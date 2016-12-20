2c81b05fb3a1ff4c31767acc7d923f952b1678de
Jonathan Guthrie <jono@perfect.org>
2016-12-20 12:15:33 -0500

    Initial commit

---
+++ Sources/main.swift
@@ -0,0 +1,33 @@
+
+
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+import PerfectSession
+import PerfectSessionCouchDB
+import CouchDBStORM
+
+let server = HTTPServer()
+
+SessionConfig.name = "TestingCouchDBDrivers"
+SessionConfig.idle = 10
+SessionConfig.couchDatabase = "perfectsessions"
+
+CouchDBConnection.host = "localhost"
+CouchDBConnection.username = "perfect"
+CouchDBConnection.password = "perfect"
+
+let sessionDriver = SessionCouchDBDriver()
+
+server.setRequestFilters([sessionDriver.requestFilter])
+server.setResponseFilters([sessionDriver.responseFilter])
+
+server.addRoutes(makeWebDemoRoutes())
+server.serverPort = 8181
+
+do {
+	// Launch the HTTP server.
+	try server.start()
+} catch PerfectError.networkError(let err, let msg) {
+	print("Network error thrown: \(err) \(msg)")
+}
